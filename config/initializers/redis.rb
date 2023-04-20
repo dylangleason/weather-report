b86cb76b2792adfd8f05dfdca0d8473567ac8bd2
Dylan Gleason <dgleason8384@gmail.com>
2023-04-20 13:11:17 -0700

    fix: be sure to set redis port explicitly via initializer

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -3,4 +3,4 @@ redis_port = ENV["REDIS_PORT"] || 6379
 
 # TODO: explore using connection_pool or the builtin Rails cache
 # wrapper. Note that this doesn't provide connection pooling.
-REDIS = Redis.new(host: redis_host)
+REDIS = Redis.new(host: redis_host, port: redis_port)
